7504f493123db5ecde63e60ba46e172aa38b04c0
louiselemaire <yogawithlouiseanne@gmail.com>
2020-04-10 16:49:57 +0200

    Add comment

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 
-""" This is comment """
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
